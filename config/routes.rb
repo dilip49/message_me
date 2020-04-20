0bc187cb7d836f27bc804f6b67276f3f8adc01a6
dilip49 <dilip.bhaidiya@gmail.com>
2020-04-20 13:06:28 +0530

    navigation links

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'sessions/new'
   root 'chatroom#index'
-  get 'login', to: 'sessions#new'
+  get 'login', to: 'sessions#new', as: :login
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
