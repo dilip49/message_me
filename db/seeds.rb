ffe4881d0576fe8b758c94c914293ec4b13b6511
dilip49 <dilip.bhaidiya@gmail.com>
2020-04-20 14:25:54 +0530

    user and message resouce

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+	# User.create(username:"dilip", password:"password")
+	# User.create(username:"Ashwin", password:"password")
+	# User.create(username:"Bhupendra", password:"password")
+	# User.create(username:"simran", password:"password")
+	# User.create(username:"poorvi", password:"password")
+	
+	Message.create(body: "hi", user_id: 1)
+	Message.create(body: "hello", user_id: 2)
