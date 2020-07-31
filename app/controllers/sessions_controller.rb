8251c2537621b7b78c1aab2f3db2d0e574404e26
dilip bhaidiya <dk319094@gmail.com>
2020-07-31 22:58:30 +0530

    fixed indentation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,11 @@
 class SessionsController < ApplicationController
   before_action :logged_in_redirect, only: [:create]
 
-  def new
-  end
+  def new; end
 
   def create
-  	user = User.find_by(username: params[:session][:username])
+    user = User.find_by(username: params[:session][:username])
+    
   	if user && user.authenticate(params[:session][:password])
   		session[:user_id] = user.id
   		flash[:success] = "You have successfully logged in"
